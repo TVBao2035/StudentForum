fa778dc26412ffc6dcca2e12bff01f7fe0f9e97d
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-09-12 21:55:45 +0700

    setup fe and be

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const app = express();
+const cookieParser= require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.listen(3033, () => {
+    console.log("Server is running.....");
+})
