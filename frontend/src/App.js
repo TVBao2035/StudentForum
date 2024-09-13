d2b3453e27152f50468192b892e9d9aef5a9f41a
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-09-13 18:17:33 +0700

    update again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bao Edit <code>src/App.js</code> and save to reload.
+          Bao 2 Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
