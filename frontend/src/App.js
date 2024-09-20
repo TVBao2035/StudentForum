176312a51e6d148d051a4bcafb1740eb71b87f12
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-09-20 10:59:28 +0700

    finish Login Page and Set up Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,18 @@ import './App.scss';
 
 import Register from './Pages/Register';
 import Login from './Pages/Login';
+import Layout from './Components/Layout';
+import Input from './Components/Input';
+import Home from './Pages/Home';
 
 
 
 function App() {
   return (
     <Routes>
+      <Route path='' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
+      </Route>
       <Route path="/login" element={<Login/>}/>
       <Route path="/register" element={<Register/>}/>
     </Routes>
