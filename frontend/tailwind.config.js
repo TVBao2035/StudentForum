b618b36243aa342adc2ac00237809b17be121e64
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-12-18 12:17:16 +0700

    up

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
- //   "./src/**/*.{js,jsx,ts,tsx}", // Đảm bảo Tailwind quét toàn bộ file trong src
-    './Components/CategoryManager/**/*.{js,jsx}',
-    './Components/CategoriesTab/**/*.{js,jsx}',
-    './Components/GroupManager/**/*.{js,jsx}',
-    './Components/GroupTab/**/*.{js,jsx}',
-    './Components/PostsGrid/**/*.{js,jsx}',
-    './Components/PostsTab/**/*.{js,jsx}',
-    './Components/UsersTab/**/*.{js,jsx}',
-    './Components/UsersTable/**/*.{js,jsx}',
-    './Pages/AdminDashboard/**/*.{js,jsx}',
+    //"./src/**/*.{js,jsx,ts,tsx}", // Đảm bảo Tailwind quét toàn bộ file trong src
+    './src/Components/CategoryManager/**/*.{js,jsx}',
+    './src/Components/CategoriesTab/**/*.{js,jsx}',
+    './src/Components/GroupManager/**/*.{js,jsx}',
+    './src/Components/GroupTab/**/*.{js,jsx}',
+    './src/Components/PostsGrid/**/*.{js,jsx}',
+    './src/Components/PostsTab/**/*.{js,jsx}',
+    './src/Components/UsersTab/**/*.{js,jsx}',
+    './src/Components/UsersTable/**/*.{js,jsx}',
+    './src/Pages/AdminDashboard/**/*.{js,jsx}',
   ],
   theme: {
     extend: {
